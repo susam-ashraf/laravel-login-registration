e9f0fc62c87a8e48e7836d50615cfd84fdeff8c0
Susam Ashraf <susamashraf@Susams-MacBook-Pro.local>
2019-10-05 23:44:19 +0600

    registration added

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\User;
+
+class RegistrationController extends Controller
+{
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+
+        $user = User::create(request(['name', 'email', 'password']));
+
+        auth()->login($user);
+
+        return redirect('/');
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
